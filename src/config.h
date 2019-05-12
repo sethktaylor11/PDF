e03404cfe21ab8c5e22ecdc87991af0d8b253962
Seth Taylor <sethktaylor11@yahoo.com>
2019-05-12 06:24:24 -0500

    Change some config variables

--- src/config.h
+++ src/config.h
@@ -20,20 +20,20 @@ const float kFloorZMax = 100.0f;
 const float kFloorY = -0.75617 - kFloorEps;
 
 // Simulation parameters
-const std::string mesh = "cylinder.1";
+const std::string mesh = "tank2.1";
 const float height = 4.0f;
 const double floorStiffness = 1.0;
 const double CoR = 0.5;
 const double timeStep = 0.005;
-const double delta = 0.2;
+const double delta = 0.6;
 const double a = 500.0;
 const double b = 500.0;
 const double damping = 0.001;
 const double criticalTension = 0.1;
 const double criticalCompression = 0.1;
-const double gravity = 0.0;
+const double gravity = 1.0;
 const double pressureStart = 0.0;
-const double pressureTimeScaling = 0.0;
+const double pressureTimeScaling = 1.0;
 const int pressurized = 2;
 
 #endif
